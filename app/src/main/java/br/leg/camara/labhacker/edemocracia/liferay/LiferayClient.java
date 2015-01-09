ac59fccb86aaccfea9f7422e8b1060a314db0915
Dirley Rodrigues <dirleyrls@gmail.com>
2015-01-09 19:23:28 -0200

    quite some refactoring of the client code

--- app/src/main/java/br/leg/camara/labhacker/edemocracia/liferay/LiferayClient.java
+++ app/src/main/java/br/leg/camara/labhacker/edemocracia/liferay/LiferayClient.java
@@ -3,32 +3,197 @@ package br.leg.camara.labhacker.edemocracia.liferay;
 import com.liferay.mobile.android.util.Validator;
 
 import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.utils.URIUtils;
 import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.FormElement;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.net.CookiePolicy;
 import java.net.CookieStore;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import br.leg.camara.labhacker.edemocracia.StringUtils;
+
+
+/**
+ * A set of helper methods for dealing with requests made through {@link(HttpURLConnection)}
+ */
+class RequestsHelper {
+    public static String readBody(HttpURLConnection connection) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String body = "";
+
+        try {
+            String line;
+            while (null != (line = reader.readLine())) {
+                body += line;
+            }
+        } finally {
+            reader.close();
+            connection.disconnect();
+        }
+
+        return body;
+    }
+
+    public static void writeFormData(HttpURLConnection connection, List<NameValuePair> formData) throws IOException {
+        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formData);
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+
+        DataOutputStream out = null;
+        try {
+            out = new DataOutputStream(connection.getOutputStream());
+            entity.writeTo(out);
+        } finally {
+            if (out != null) {
+                try {
+                    out.flush();
+                    out.close();
+                } catch (IOException e) {
+                    // NOOP
+                }
+            }
+        }
+    }
+}
+
+
+/**
+ * A helper class for dealing with Liferay's authentication system.
+ *
+ * Here are most of the methods you'll use to authenticate a LiferayClient session,
+ * grab tokens, etc. Most of these methods are only used by LiferayClient.
+ */
+class AuthenticationHelper {
+
+    public static boolean authenticate(LiferayClient client, String username, String password) throws IOException {
+        client.setupCookieHandler();
+
+        FormElement form = getLoginForm(client);
+
+        List<NameValuePair> loginFormData = getLoginFormData(form);
+
+        for (NameValuePair item : loginFormData) {
+            String name = item.getName();
+            if (name.endsWith("login")) {
+                loginFormData.set(loginFormData.indexOf(item), new BasicNameValuePair(name, username));
+            } else if (name.endsWith("password")) {
+                loginFormData.set(loginFormData.indexOf(item), new BasicNameValuePair(name, password));
+            } else if (name.endsWith("rememberMe")) {
+                loginFormData.set(loginFormData.indexOf(item), new BasicNameValuePair(name, "true"));
+            }
+        }
+
+        URL loginUrl = new URL(form.attr("action"));
+        HttpURLConnection connection = (HttpURLConnection) loginUrl.openConnection();
+
+        RequestsHelper.writeFormData(connection, loginFormData);
+
+        String loginBody = RequestsHelper.readBody(connection);
+
+        boolean success = checkIsAuthenticated(loginBody);
+
+        if (checkIsAuthenticated(loginBody)) {
+            AuthenticationToken token = extractAuthToken(loginBody);
+            client.setToken(token);
+        }
+
+        return success;
+    }
+
+    public static AuthenticationToken fetchAuthenticationToken(LiferayClient client) throws IOException, URISyntaxException {
+        client.setupCookieHandler();
+
+        URL url = client.createURL("/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        String body = RequestsHelper.readBody(connection);
+
+        return extractAuthToken(body);
+    }
+
+    public static boolean isAuthenticated(LiferayClient client) throws IOException {
+        client.setupCookieHandler();
+
+        URL url = client.createURL("/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        String body = RequestsHelper.readBody(connection);
+
+        return checkIsAuthenticated(body);
+    }
+
+    private static boolean checkIsAuthenticated(String body) {
+        // FIXME Implement a proper logic to check if the user is authenticated
+        return body.contains("/c/portal/logout");
+    }
+
+    private static AuthenticationToken extractAuthToken(String content) {
+        Pattern pattern = Pattern.compile("authToken\\s*=\\s*(\"[^\"]+\"|'[^']+')");
+        Matcher matcher = pattern.matcher(content);
+
+        if (matcher.find()) {
+            String token = StringUtils.strip(matcher.group(1), "'\"");
+            return new AuthenticationToken(token);
+        } else {
+            return null;
+        }
+    }
+
+    private static FormElement getLoginForm(LiferayClient client) throws IOException {
+        URL url = client.createURL("/");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        String body = RequestsHelper.readBody(connection);
+
+        Document document = Jsoup.parse(body, "UTF-8");
+
+        return (FormElement) document.getElementsByTag("form").first();
+    }
+
+    private static List<NameValuePair> getLoginFormData(FormElement form) {
+        List<NameValuePair> formData = new ArrayList<>();
+
+        for (Connection.KeyVal item : form.formData()) {
+            formData.add(new BasicNameValuePair(item.key(), item.value()));
+        }
+
+        return formData;
+    }
+}
 
 public class LiferayClient {
 
     private static final int wsPort = 8080;
     private static final String wsProtocol = "http";
-    private static final String wsHost = "192.168.1.11";
+    private static final String wsHost = "192.168.25.9";
     private static final String wsPath = "/api/jsonws";
 
     private AuthenticationToken token;
@@ -49,139 +214,6 @@ public class LiferayClient {
         this.cookies = cookies;
     }
 
-    public static boolean isTokenUsable(AuthenticationToken token) {
-        return token != null && Validator.isNotNull(token.getToken()) && !token.isExpired();
-    }
-
-    public boolean authenticate(String username, String password) throws IOException {
-        token = AuthenticationHelper.authenticateAndGetToken(username, password, cookies);
-        return isTokenUsable(token);
-    }
-
-    public boolean isAuthenticated() throws IOException {
-        return AuthenticationHelper.isAuthenticated(cookies);
-    }
-
-    public JSONArray listGroups(int companyId) throws ServerException, IOException, URISyntaxException {
-        List<NameValuePair> args = new ArrayList<NameValuePair>();
-
-        args.add(new BasicNameValuePair("companyId", Integer.toString(companyId)));
-        args.add(new BasicNameValuePair("start", Integer.toString(-1)));
-        args.add(new BasicNameValuePair("end", Integer.toString(-1)));
-        args.add(new BasicNameValuePair("name", "%"));
-        args.add(new BasicNameValuePair("description", "%"));
-        args.add(new BasicNameValuePair("params", null));
-
-        // Always add token last, and make sure it exists.
-        args.add(new BasicNameValuePair("p_auth", getValidAuthenticationToken().getToken()));
-
-        return this.call("/group/search", args);
-    }
-
-    protected JSONArray call(String method, List<NameValuePair> args) throws ServerException, IOException, URISyntaxException {
-
-        // XXX Always set the default cookie handler to make sure cookies are correctly
-        // stored in our internal cookie store.
-        CookieHandler.setDefault(new CookieManager(cookies, CookiePolicy.ACCEPT_ALL));
-
-        if (!isTokenUsable(token)) {
-            throw new ServerException("Invalid authentication token (locally)");
-        }
-
-        URL url = this.buildMethodURL(method, args);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        // Handle response codes
-        int status = connection.getResponseCode();
-
-        if ((status == HttpURLConnection.HTTP_MOVED_PERM) ||
-                (status == HttpURLConnection.HTTP_MOVED_TEMP) ||
-                (status == HttpURLConnection.HTTP_SEE_OTHER)) {
-            throw new RedirectException(connection.getHeaderField("Location"));
-        }
-
-        if (status == HttpURLConnection.HTTP_UNAUTHORIZED) {
-            throw new ServerException("Authentication failed");
-        }
-
-        if (status != HttpURLConnection.HTTP_OK) {
-            throw new ServerException("Request failed with code " + status);
-        }
-
-        // Read the response content
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-        String content = "";
-
-        try {
-            String line;
-            while (null != (line = reader.readLine())) {
-                content += line;
-            }
-        } finally {
-            reader.close();
-            connection.disconnect();
-        }
-
-
-        JSONArray result = null;
-
-        try {
-            if (isJSONObject(content)) {
-                // Check if its an exception
-                JSONObject o = new JSONObject(content);
-
-                if (o.has("exception")) {
-                    throw new ServerException(o.getString("exception"));
-                } else {
-                    // XXX FIXME Is this `else` branch really necessary? I mean, can it happen?
-                    result = new JSONArray();
-                    result.put(o);
-                }
-            } else {
-                result = new JSONArray(content);
-            }
-        } catch (JSONException e) {
-            throw new ServerException("Invalid response. " + e.toString(), e);
-        }
-
-        return result;
-    }
-
-    private AuthenticationToken fetchAuthenticationToken() throws IOException, URISyntaxException {
-        CookieHandler.setDefault(new CookieManager(cookies, CookiePolicy.ACCEPT_ALL));
-
-        URL url = createURL("/");
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-        // Where the content of the request will be saved
-        String content = "";
-
-        try {
-            String line;
-            while (null != (line = reader.readLine())) {
-                content += line;
-            }
-        } finally {
-            reader.close();
-            urlConnection.disconnect();
-        }
-
-        return AuthenticationHelper.extractAuthToken(content);
-    }
-
-    private AuthenticationToken getValidAuthenticationToken() throws IOException, URISyntaxException {
-        if (!isTokenUsable(token)) {
-            token = fetchAuthenticationToken();
-        }
-        return token;
-    }
-
-    private static Boolean isJSONObject(String s) {
-        return Validator.isNotNull(s) && s.startsWith("{");
-    }
-
     public CookieStore getCookies() {
         return cookies;
     }
@@ -198,15 +230,107 @@ public class LiferayClient {
         this.token = token;
     }
 
-    private static URL createURL(String path) throws MalformedURLException {
+    public boolean authenticate(String username, String password) throws IOException {
+        return AuthenticationHelper.authenticate(this, username, password);
+    }
+
+    public boolean isAuthenticated() throws IOException {
+        return AuthenticationHelper.isAuthenticated(this);
+    }
+
+    public JSONArray listGroups(int companyId) throws ServerException, IOException, URISyntaxException {
+        List<NameValuePair> args = new ArrayList<>();
+
+        args.add(new BasicNameValuePair("companyId", Integer.toString(companyId)));
+        args.add(new BasicNameValuePair("start", Integer.toString(-1)));
+        args.add(new BasicNameValuePair("end", Integer.toString(-1)));
+        args.add(new BasicNameValuePair("name", "%"));
+        args.add(new BasicNameValuePair("description", "%"));
+        args.add(new BasicNameValuePair("params", null));
+
+        // Always add token last, and make sure it exists.
+        args.add(new BasicNameValuePair("p_auth", getValidAuthenticationToken().getToken()));
+
+        return this.call("/group/search", args);
+    }
+
+    /**
+     * Must be called before doing any requests to URLs from this client. This grants that the
+     * cookies will be updated on the client's cookie store.
+     */
+    public void setupCookieHandler() {
+        CookieManager cookieManager = new CookieManager(this.cookies, CookiePolicy.ACCEPT_ALL);
+        CookieHandler.setDefault(cookieManager);
+    }
+
+    protected JSONArray call(String method, List<NameValuePair> args) throws ServerException, IOException, URISyntaxException {
+        setupCookieHandler();
+
+        if (!isTokenUsable(token)) {
+            throw new ServerException("Invalid or expired authentication token");
+        }
+
+        URL url = createMethodURL(method, args);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        int status = connection.getResponseCode();
+
+        if ((status == HttpURLConnection.HTTP_MOVED_PERM) ||
+                (status == HttpURLConnection.HTTP_MOVED_TEMP) ||
+                (status == HttpURLConnection.HTTP_SEE_OTHER)) {
+            throw new RedirectException(connection.getHeaderField("Location"));
+        }
+
+        if (status == HttpURLConnection.HTTP_UNAUTHORIZED) {
+            throw new ServerException("Unauthorized");
+        }
+
+        if (status != HttpURLConnection.HTTP_OK) {
+            throw new ServerException("Request failed with code " + status);
+        }
+
+        String content = RequestsHelper.readBody(connection);
+
+        JSONArray result;
+        try {
+            if (isJSONObject(content)) {
+                JSONObject o = new JSONObject(content);
+
+                if (o.has("exception")) {
+                    throw new ServerException(o.getString("exception"));
+                } else {
+                    // XXX Return any other "single object response" into an Array containing
+                    // the response object. I don't really know if this is necessary, tho.
+                    result = new JSONArray();
+                    result.put(o);
+                }
+            } else {
+                result = new JSONArray(content);
+            }
+        } catch (JSONException e) {
+            throw new ServerException("Invalid response: " + e.toString(), e);
+        }
+
+        return result;
+    }
+
+    public static URL createMethodURL(String method, List<NameValuePair> args) throws URISyntaxException, MalformedURLException {
+        String queryString = URLEncodedUtils.format(args, "UTF-8");
+
+        String path = StringUtils.strip(wsPath, "/") + "/" + StringUtils.strip(method) + "/";
+
+        return createURL(path, queryString);
+    }
+
+    public static URL createURL(String path) throws MalformedURLException {
         return createURL(path, null);
     }
 
-    private static URL createURL(String path, String query) throws MalformedURLException {
+    public static URL createURL(String path, String query) throws MalformedURLException {
         return createURL(path, query, null);
     }
 
-    private static URL createURL(String path, String query, String fragment) throws MalformedURLException {
+    public static URL createURL(String path, String query, String fragment) throws MalformedURLException {
         try {
             return URIUtils.createURI(wsProtocol, wsHost, wsPort, path, query, fragment).toURL();
         } catch (URISyntaxException e) {
@@ -214,26 +338,18 @@ public class LiferayClient {
         }
     }
 
-    protected URL buildMethodURL(String method, List<NameValuePair> args) throws URISyntaxException, MalformedURLException {
-        String queryString = URLEncodedUtils.format(args, "UTF-8");
+    private static boolean isTokenUsable(AuthenticationToken token) {
+        return token != null && Validator.isNotNull(token.getToken()) && !token.isExpired();
+    }
 
-        // Construct the Path
-        String path = wsPath;
-        while (path.startsWith("/")) {
-            path = path.substring(1);
-        }
-        while (path.endsWith("/")) {
-            path = path.substring(0, path.length() - 1);
-        }
-        String method_ = method;
-        while (method_.startsWith("/")) {
-            method_ = method_.substring(1);
-        }
-        path = path + "/" + method_;
-        while (path.endsWith("/")) {
-            path = path.substring(0, path.length() - 1);
-        }
+    private static boolean isJSONObject(String s) {
+        return Validator.isNotNull(s) && s.startsWith("{");
+    }
 
-        return createURL(path, queryString);
+    private AuthenticationToken getValidAuthenticationToken() throws IOException, URISyntaxException {
+        if (!isTokenUsable(token)) {
+            token = AuthenticationHelper.fetchAuthenticationToken(this);
+        }
+        return token;
     }
 }
