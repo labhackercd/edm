d03d9796d7a6d1a33105e59d570ee781b44fb6c0
Dirley Rodrigues <dirleyrls@gmail.com>
2015-01-09 19:27:29 -0200

    rename test method

--- app/src/androidTest/java/br/leg/camara/labhacker/edemocracia/ApplicationTest.java
+++ app/src/androidTest/java/br/leg/camara/labhacker/edemocracia/ApplicationTest.java
@@ -14,18 +14,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    public void testSomeStuff() {
-        Time time = new Time();
-        time.set(1, 1, 1, 31, 0, 2014);
-
-        time.monthDay += 1;
-        time.normalize(true);
-
-        assertEquals(time.month, 1);
-        assertEquals(time.monthDay, 1);
-    }
-
-    public void testTheShitOutOfIt() throws Exception {
+    public void testMethodInvocationWorks() throws Exception {
         LiferayClient client = new LiferayClient();
 
         assertTrue(client.authenticate("dirleyrls@gmail.com", "12345"));
