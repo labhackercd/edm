3ecec21d4146deaeced15d7b84e9a75611549b8b
Dirley Rodrigues <dirleyrls@gmail.com>
2015-01-14 00:04:09 -0200

    rename main menu resource file

--- app/src/main/java/br/leg/camara/labhacker/edemocracia/MainActivity.java
+++ app/src/main/java/br/leg/camara/labhacker/edemocracia/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends Activity implements GroupListFragment.OnGroupS
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_group_list, menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
