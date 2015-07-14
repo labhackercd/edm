2737d948f46f5a546d49e29ae6491c931413e033
Dirley Rodrigues <dirleyrls@gmail.com>
2015-07-14 11:35:31 -0300

    Add GPLv3 license.

--- app/src/main/java/net/labhackercd/nhegatu/account/AccountAuthenticator.java
+++ app/src/main/java/net/labhackercd/nhegatu/account/AccountAuthenticator.java
@@ -1,3 +1,20 @@
+/*
+ * This file is part of Nhegatu, the e-Demoracia Client for Android.
+ *
+ * Nhegatu is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Nhegatu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Nhegatu.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.labhackercd.nhegatu.account;
 
 import android.accounts.AbstractAccountAuthenticator;
